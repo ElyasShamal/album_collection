9297d99eb76068c270a68e6569705215418cb6f4
Elyas <Elyas.shamal2@gmail.com>
2023-06-24 23:53:21 -0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -8,14 +8,9 @@ function App() {
     <div className="App">
       <h1>My Music Album Collection</h1>
       <div className="albums">
-        <Album album={albumCollection[0]} />
-        <Album album={albumCollection[1]} />
-        <Album album={albumCollection[2]} />
-        <Album album={albumCollection[3]} />
-        <Album album={albumCollection[4]} />
-        <Album album={albumCollection[5]} />
-        <Album album={albumCollection[6]} />
-        <Album album={albumCollection[7]} />
+        {albumCollection.map((album) => (
+          <Album album={album} />
+        ))}
       </div>
     </div>
   );
